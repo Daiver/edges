96568c1299cb9a8ce3e5b90f3bde854f86ec4984
daiver <ra22341@ya.ru>
2014-03-02 10:35:42 +0400

    preparing for pca

---
+++ misc/CMakeFiles/pca_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pca_test.dir/pca_test.cpp.o"
+  "pca_test.pdb"
+  "pca_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pca_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
