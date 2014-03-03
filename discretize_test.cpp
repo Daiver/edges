6db73af7dcb7c5bd2421a7968a850bc316364760
daiver <ra22341@ya.ru>
2014-03-03 23:39:45 +0400

    cut patches added

--- discretize_test.cpp
+++ discretize_test.cpp
@@ -15,12 +15,35 @@ void read_imgList(const std::string& filename, std::vector<cv::Mat>* images) {
     }
 }
 
+void cutPatchesFromImage(cv::Mat img, std::vector<cv::Mat>* patches){
+    int w = 16;
+    for (int i = 0; i < img.rows; i+=100){
+        for (int j = 0; j < img.cols; j+=100){
+            cv::Mat tileCopy = img(cv::Range(i, std::min(i + w, img.rows)),
+                 cv::Range(j, std::min(j + w, img.cols))).clone();
+            if (tileCopy.rows != w || tileCopy.cols != w) continue;
+                patches->push_back(tileCopy);
+        }
+    }
+}
+
 int main(){
     std::vector<cv::Mat> images;
     read_imgList("images.txt", &images);
     for(auto& img : images){
         cv::imshow("", img);
-        cv::waitKey();
+        for(int i = 0 ; i < img.rows; i++){
+            for(int j = 0 ; j < img.rows; j++){
+                printf("%d ", img.at<uchar>(i, j));
+            }
+            printf("\n");
+        }
+        std::vector<cv::Mat> patches;
+        cutPatchesFromImage(img, &patches);
+        for(auto &tileCopy : patches){
+            cv::imshow("1", tileCopy);
+            cv::waitKey();
+        }
     }
     return 0;
 }
