936ca4cba6d2fc4e297fb78c74b1f972f7972d57
daiver <ra22341@ya.ru>
2014-03-03 22:47:37 +0400

    minor changes

---
+++ CMakeFiles/bin/discretize_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/bin/discretize_test.dir/discretize_test.cpp.o"
+  "bin/discretize_test.pdb"
+  "bin/discretize_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/bin/discretize_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ discretize_test.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+int main(){
+    return 0;
+}
