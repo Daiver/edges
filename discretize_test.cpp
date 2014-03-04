2cf4c1486cee8ae796f04c0992b065e54daf607c
daiver <ra22341@ya.ru>
2014-03-04 08:01:20 +0400

    prepare for discretization

--- discretize_test.cpp
+++ discretize_test.cpp
@@ -27,23 +27,49 @@ void cutPatchesFromImage(cv::Mat img, std::vector<cv::Mat>* patches){
     }
 }
 
+/*std::vector<int> getRandIndxs(int size){
+    std::vector<int> indxs(size);
+    for(int i = 0; i < size; i++) indxs[i] = i;
+    for(int i = 0; i < size; i++) swap(&indxs[i], &indxs[rand() % size]);
+    return indxs;
+}*/
+
+void selectFeaturesFromPatches(std::vector<cv::Mat>& images){
+    std::vector<int> indxs;
+    for(int i = 0; i < 256; i++){
+        indxs.push_back((int)(rand() % (256*256)));
+    }
+    std::vector<std::vector<int>> zs(images.size());
+    for(int i = 0; i < images.size(); i++){
+        for(int j = 0; j < 256; j++){
+            zs[i].push_back(images[i].data[indxs[j]/256] == images[i].data[indxs[j]%256]);
+            printf("%d %d %d\n", zs[i][j], images[i].data[indxs[j]/256], images[i].data[indxs[j]%256]);
+        }
+    }
+    //for(auto &i : indxs){
+    //    printf("%d %d %d\n", i, i / 256, i % 256);
+    //} 
+}
+
 int main(){
     std::vector<cv::Mat> images;
+    std::vector<cv::Mat> patches;
     read_imgList("images.txt", &images);
     for(auto& img : images){
         cv::imshow("", img);
-        for(int i = 0 ; i < img.rows; i++){
+        cutPatchesFromImage(img, &patches);
+        //for(auto &tileCopy : patches){
+            //cv::imshow("1", tileCopy);
+            //cv::waitKey();
+        //}
+    }
+    selectFeaturesFromPatches(patches);
+    return 0;
+}
+        /*for(int i = 0 ; i < img.rows; i++){
             for(int j = 0 ; j < img.rows; j++){
                 printf("%d ", img.at<uchar>(i, j));
             }
             printf("\n");
-        }
-        std::vector<cv::Mat> patches;
-        cutPatchesFromImage(img, &patches);
-        for(auto &tileCopy : patches){
-            cv::imshow("1", tileCopy);
-            cv::waitKey();
-        }
-    }
-    return 0;
-}
+        }*/
+
