1958bb1ca0677bd558629431f735a2505b4785fb
daiver <ra22341@ya.ru>
2014-03-05 01:07:41 +0400

    yeah

--- discretize.cpp
+++ discretize.cpp
@@ -3,19 +3,21 @@
 int selectFeaturesFromPatches(std::vector<cv::Mat>& images, std::vector<int> *hs){
     std::vector<int> indxs;
     for(int i = 0; i < 256; i++){
-        indxs.push_back((int)(rand() % (256*256)));
+        int x = (int)rand() % (256*256);
+        while((x/256) == (x%256))
+            x = (int)rand() % (256*256);
+        indxs.push_back(x);
     }
     //std::vector<std::vector<double>> zs(images.size());
     cv::Mat zs(images.size(), 256, CV_32F);
     for(int i = 0; i < images.size(); i++){
         for(int j = 0; j < 256; j++){
-            zs.at<float>(i, j) = images[i].data[indxs[j]/256] == images[i].data[indxs[j]%256];
-            //zs[i].push_back(images[i].data[indxs[j]/256] == images[i].data[indxs[j]%256]);
-            //printf("%d %d %d\n", zs[i][j], 
-            //        images[i].data[indxs[j] / 256], 
-            //        images[i].data[indxs[j] % 256]);
+            zs.at<float>(i, j) = 
+                images[i].data[indxs[j]/256] == images[i].data[indxs[j]%256];
         }
     }
+
+    std::vector<int> indx_to_delete;
     for(int j = 0; j < 256; j++){
         float sum = 0;
         for(int i = 0; i < images.size(); i++){
@@ -30,20 +32,63 @@ int selectFeaturesFromPatches(std::vector<cv::Mat>& images, std::vector<int> *hs
             zs.at<float>(i, j) -= norm_cnst;
             //printf("%0.2f ", zs.at<float>(i, j));
         }
+
+        sum = 0;
+        for(int i = 0; i < images.size(); i++){
+            sum += zs.at<float>(i, j);
+        }
+        if(sum == 0){
+            indx_to_delete.push_back(j);
+        }
         //printf("\n");
     }
-    cv::PCA pca(zs, cv::Mat(), CV_PCA_DATA_AS_ROW, 0.85);
+    if(indx_to_delete.size() > 0){
+        cv::Mat tmp_zs(images.size(), 256 - indx_to_delete.size(), CV_32F);
+        int counter = 0;
+        for(int j = 0; j < 256; j++){
+            if(std::find(indx_to_delete.begin(), indx_to_delete.end(),j) 
+                    == indx_to_delete.end()){
+                for(int i = 0; i < images.size(); i++){
+                    tmp_zs.at<float>(i, counter) = zs.at<float>(i, j);
+                }
+                //tmp_zs.col(counter) = zs.col(j);
+                counter += 1;
+            }
+        }
+        zs = tmp_zs;
+    }
+    for(int j = 0; j < 256 - indx_to_delete.size(); j++){
+        float sum = 0;
+        for(int i = 0; i < images.size(); i++){
+            sum += zs.at<float>(i, j);
+        }
+        if(sum == 0){
+            printf("zero col %d\n", j);
+        }
+    }
+
+    cv::PCA pca(zs, cv::Mat(), CV_PCA_DATA_AS_ROW, 0.89);
     printf("PCA # %d\n", pca.eigenvectors.rows);
     cv::Mat zs2(images.size(), pca.eigenvectors.rows, CV_32F);
     for(int i = 0; i < images.size(); i++){
-        zs2.row(i) = pca.project(zs.row(i));
-        //for(int j = 0; j < pca.eigenvectors.rows; j++){
-        //    //printf("%f ", zs2.at<float>(i, j));
+        auto res = pca.project(zs.row(i));
+        for(int j = 0; j < pca.eigenvectors.rows; j++){
+            zs2.at<float>(i, j) = res.at<float>(0,j);
+        }
+        double mean = cv::mean(images[i])[0];
+        double mean2 = cv::mean(zs.row(i))[0];
+        double mean3 = cv::mean(zs2.row(i))[0];
+        printf("+ %f %f %f\n", mean/images[i].at<uchar>(0,0), mean2, mean3);
+        for(int j = 0; j < 10; j++){
+            printf("%f ", zs.at<float>(i, j));
+        }
+        printf("\n");
+        //printf("%d>", i);
+        //for(int j = 0; j < zs2.row(i).cols; j++){
+        //    printf("%f ", zs2.at<float>(i, j));
         //}
-        //printf("\n\n");
+        //printf("\n");
     }
-
-    //std::vector<long> hs(images.size(), 0);
     int boundary = std::min(pca.eigenvectors.rows, 10);
     std::vector<int> hs2(images.size(), 0);
     for(int i = 0; i < images.size(); i++){
@@ -54,7 +99,7 @@ int selectFeaturesFromPatches(std::vector<cv::Mat>& images, std::vector<int> *hs
     }
     int hash_table_size = pow(2, boundary);
     int *hash_table = new int[hash_table_size];
-    memset(hash_table, -1, sizeof(int) * hash_table_size);
+    for(int i = 0; i < hash_table_size; i++) hash_table[i] = -1;
     int counter = 0;
     for(auto x : hs2){
         if (hash_table[x] == -1){
@@ -68,9 +113,6 @@ int selectFeaturesFromPatches(std::vector<cv::Mat>& images, std::vector<int> *hs
     printf("counter %d\n", counter);
     delete [] hash_table;
     return counter;
-    //for(auto &i : indxs){
-    //    printf("%d %d %d\n", i, i / 256, i % 256);
-    //} 
 }
 
 
--- discretize_test.cpp
+++ discretize_test.cpp
@@ -1,13 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <iostream>
-#include <fstream>
-#include <sstream>
 #include <vector>
-#include <string>
 #include "discretize.h"
 #include "common.h"
 
@@ -23,18 +18,34 @@ int main(){
     std::vector<int> hs(patches.size(), 0);
     int num_of_classes = selectFeaturesFromPatches(patches, &hs);
     for(int i = 0; i < num_of_classes; i++){
+        int counter = 0;
         for(int j = 0; j < hs.size(); j++){
             if (i != hs[j]) continue;
+            if (counter > 40) continue;
             char name[100];
             sprintf(name, "a %d", j);
+            int sum = 0;
+            float disp = 0;
+            for(int r = 0; r < patches[j].rows; r++){
+                for(int c = 0; c < patches[j].cols; c++){
+                    sum += patches[j].at<uchar>(r,c);
+                }
+            }
+            for(int r = 0; r < patches[j].rows; r++){
+                for(int c = 0; c < patches[j].cols; c++){
+                    disp += pow(patches[j].at<uchar>(r,c) - ((float)sum/(256)), 2);
+                }
+            }
+            printf("%d %d %f\n", j, sum, disp/(16*16));
             cv::Mat tmp2;
             cv::normalize(patches[j], tmp2, 0, 255, cv::NORM_MINMAX, CV_8UC1);
             cv::pyrUp(tmp2, tmp2);
             cv::pyrUp(tmp2, tmp2);
-            cv::normalize(tmp2, tmp2, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+            //cv::normalize(tmp2, tmp2, 0, 255, cv::NORM_MINMAX, CV_8UC1);
             cv::imshow(name, tmp2);
-            printf(">%d\n", i);
+            counter += 1;
         }
+        printf(">%d\n", i);
         printf("\n");
         cv::waitKey();
 
